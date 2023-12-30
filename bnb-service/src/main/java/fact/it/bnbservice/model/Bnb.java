4a001a429d7e11f565c5f995f48c333c7825259a
BRSimons15 <91054314+BRSimons15@users.noreply.github.com>
2023-12-30 22:26:36 +0100

    more changes to bnb

--- bnb-service/src/main/java/fact/it/bnbservice/model/Bnb.java
+++ bnb-service/src/main/java/fact/it/bnbservice/model/Bnb.java
@@ -7,26 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="bnb")
+@Table(name = "bnb")
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Bnb {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     @ElementCollection
-    private List<String> roomCodes;
+    private List<String> roomCodes = new ArrayList<>();
 
     private String city;
     private String postcode;
     private String address;
 
-    // Add a room code to list
+    // Add a room code to the list
     public void addRoomCode(String roomCode) {
-        if (roomCodes == null) { roomCodes = new ArrayList<>(); }
+        if (roomCodes == null) {
+            roomCodes = new ArrayList<>();
+        }
         roomCodes.add(roomCode);
     }
 }
+
